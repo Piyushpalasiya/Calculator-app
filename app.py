db03b736b8318b33d85bf0623be2b070108bf498
Piyushpalasiya <piyush.rathodd@gmail.com>
2024-09-12 18:00:24 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from calc_function import do_addition, do_subtraction
+
+def main():
+    print('Welcome to the Calculator App')
+    print("""
+Select the function from the given options:
+1. Add
+2. Subtract
+""")
+    
+    user_input = input("Select the function: ")
+
+    a = int(input("Value of A: "))
+    b = int(input("Value of B: "))
+
+    if user_input == "1":
+        result = a + b
+    elif user_input == "2":
+        result = a - b
+    else:
+        result = "Invalid option selected"
+
+    print('Result:', result)
+
+if __name__ == "__main__":
+    main()
---
+++ calc_function.py
@@ -0,0 +1,10 @@
+# additiin
+
+def do_addition(a:int,b:int):
+    return a + b
+
+# subtraction  "subtr": unknown word
+
+def do_subtraction(a,b):
+    return a - b
+
