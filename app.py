8c43c9b34af2ae3bd317e0c4b4fac900382a1767
Piyushpalasiya <piyush.rathodd@gmail.com>
2024-09-13 15:39:34 +0530

    Added feature for division

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from calc_function import do_addition, do_subtraction
+from calc_function import do_addition, do_subtraction, do_division
 from multiply import do_multiplication
-
+#from area import calculate_area_reactangle
 
 def main():
     print('Welcome to the Calculator App')
@@ -8,7 +8,8 @@ def main():
 Select the function from the given options:
 1. Add
 2. Subtract
-3. multiplication
+3. Multiplication
+4. Area
 """)
     
     user_input = input("Select the function: ")
@@ -17,11 +18,16 @@ Select the function from the given options:
     b = int(input("Value of B: "))
 
     if user_input == "1":
-        result = a + b
+        result = do_addition (a,b)
+
     elif user_input == "2":
-        result = a - b
+        result = do_subtraction (a,b)
+
     elif user_input == "3":
-        result = a * b
+        result = do_multiplication (a,b)
+
+    elif user_input == "4":
+        result = do_division(a,b)
     else:
         result = "Invalid option selected"
 
--- calc_function.py
+++ calc_function.py
@@ -4,7 +4,11 @@ def do_addition(a:int,b:int):
     return a + b
 
 # subtraction  "subtr": unknown word
-
 def do_subtraction(a,b):
     return a - b
 
+def do_division(a,b):
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        return "Connot divide by zero"
